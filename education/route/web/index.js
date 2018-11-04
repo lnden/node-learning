3026c3c9986f1d2a48bbf60cfba53c288f439ef9
ding lindong <258231674@qq.com>
2018-11-04 22:39:15 +0800

    add

---
+++ education/route/web/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mysql = require('mysql');
+
+var db = mysql.createPool({host:'localhost',user:'root',password:'123456',database:'learn'})
+
+module.exports = function(){
+	let router = express.Router();
+	
+	router.get('/get_banners',(req,res)=>{
+		db.query(`SELECT * FROM banner_table`,(err,data)=>{
+			if(err){
+				console.error(err);
+				res.status(500).send('database error').end();
+			}else{
+				console.log(data)
+				res.status(200).send(data).end();
+			}
+		})
+	})
+	
+	// router.get('/get_custom',(req,res)=>{
+	// 	res.send('我是web').end();
+	// })
+	return router
+}
+
+
+// 前端接口文档
+// 1. get_banners   =>    [{ID,title,description,href}]
+// 2. get_custom    =>    [{ID,title,descriptioon,src}]
