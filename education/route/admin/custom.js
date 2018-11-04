1d68b82ab0f3e9ddab87a9900506805e834a454c
ding lindong <258231674@qq.com>
2018-11-04 22:11:05 +0800

    完成上传图片 用户评价问题

--- education/route/admin/custom.js
+++ education/route/admin/custom.js
@@ -16,8 +16,25 @@ module.exports = function(){
 	const router = express.Router();
 
 	router.get('/',(req,res)=>{
-		switch(req.query.act){
+		switch(req.query.act){	
 			case 'mod':
+				db.query(`SELECT * FROM custom_evaluation_table WHERE ID=${req.query.id}`,(err,data)=>{
+					if(err){
+						console.error(err);
+						res.status(500).send('database error').end();
+					}else if(data.length==0){
+						res.status(404).send('no this custom evaluation').end();
+					}else{
+						db.query(`SELECT * FROM custom_evaluation_table`,(err,data)=>{
+							if(err){
+								console.error(err);
+								res.status(500).send('database error').end()
+							}else{
+								res.render('admin/custom.ejs',{custom:data,mod_data:data[0]});
+							}
+						})
+					}
+				})
 				break;
 			case 'del':
 				db.query(`SELECT * FROM custom_evaluation_table WHERE ID=${req.query.id}`,(err,data)=>{
@@ -64,32 +81,82 @@ module.exports = function(){
 		var title = req.body.title;
 		var description = req.body.description;
 
-		// console.log(req.files)
-		var ext = pathLib.parse(req.files[0].originalname).ext;
-		var oldPath = req.files[0].path
-		var newPath = req.files[0].path+ext;
-		var newFileName = req.files[0].filename+ext;
+		if(req.files[0]){
+			// console.log(req.files)
+			var ext = pathLib.parse(req.files[0].originalname).ext;
+			var oldPath = req.files[0].path
+			var newPath = req.files[0].path+ext;
+			var newFileName = req.files[0].filename+ext;
+		}else{
+			var newFileName = null;
+		}
+	
+		if(newFileName){
+			fs.rename(oldPath,newPath,(err)=>{
+				if(err){
+					console.error(err);
+					res.status(500).send('file opration error').end();
+				}else{
+					if(req.body.mod_id){	//修改
+						//先删除老的
+						db.query(`SELECT * FROM custom_evaluation_table WHERE ID=${req.body.mod_id}`,(err,data)=>{
+							if(err){
+								console.error(err);
+								res.status(500).send('database error').end();
+							}else if(data.length==0){
+								res.status(404).send('old file not fount').end()
+							}else{
+								fs.unlink('static/upload/'+data[0].src,(err)=>{
+									if(err){
+										console.error(err);
+										res.status(500).send('file opration error').end();
+									}else{
+										db.query(`UPDATE custom_evaluation_table SET title='${title}',description='${description}',src='${newFileName}' WHERE ID=${req.body.mod_id}`,(err,data)=>{
+											if(err){
+												console.error(err);
+												res.status(500).send('database error').end();
+											}else{
+												res.redirect('/admin/custom');
+											}
+										})
+									}
+								})
+							}
+						})
+					}else{	//添加
+						db.query(`INSERT INTO custom_evaluation_table (title,description,src) VALUES ('${title}','${description}','${newFileName}')`,(err,data)=>{
+							if(err){
+								console.error(err);
+								res.status(500).send('database error').end();
+							}else{
+								res.redirect('/admin/custom');
+							}
+						})
+					}
 
-		fs.rename(oldPath,newPath,(err)=>{
-			if(err){
-				res.status(500).send('file opration error').end();
-			}else{
-				if(req.body.mod_id){	//修改
-
-				}else{	//添加
-					db.query(`INSERT INTO custom_evaluation_table (title,description,src) VALUES ('${title}','${description}','${newFileName}')`,(err,data)=>{
-						if(err){
-							console.error(err);
-							res.status(500).send('database error').end();
-						}else{
-							res.redirect('/admin/custom');
-						}
-					})
 				}
+			});
+		}else{
+			if(req.body.mod_id){	//修改
+				db.query(`UPDATE custom_evaluation_table SET title='${title}',description='${description}' WHERE ID=${req.body.mod_id}`,(err,data)=>{
+					if(err){
+						console.error(err);
+						res.status(500).send('database error').end();
+					}else{
+						res.redirect('/admin/custom');
+					}
+				})
+			}else{	//添加
+				db.query(`INSERT INTO custom_evaluation_table (title,description,src) VALUES ('${title}','${description}','${newFileName}')`,(err,data)=>{
+					if(err){
+						console.error(err);
+						res.status(500).send('database error').end();
+					}else{
+						res.redirect('/admin/custom');
+					}
+				})
 			}
-		})
-
+		}
 	})
-
 	return router;
 }
